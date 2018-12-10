82faef0524e815869b1a4122ce087b2b408be0a2
angshu <angshuonline@gmail.com>
2018-12-10 19:12:49 +0530

    updated to send basic order info

--- omod/src/main/java/org/bahmni/module/example/web/controller/ExampleController.java
+++ omod/src/main/java/org/bahmni/module/example/web/controller/ExampleController.java
@@ -2,6 +2,11 @@ package org.bahmni.module.example.web.controller;
 
 
 import org.bahmni.module.example.service.ExampleService;
+import org.openmrs.Order;
+import org.openmrs.Patient;
+import org.openmrs.Visit;
+import org.openmrs.api.PatientService;
+import org.openmrs.api.VisitService;
 import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.webservices.rest.web.v1_0.controller.BaseRestController;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
-import java.util.List;
+import java.util.*;
 
 
 @Controller
@@ -17,18 +22,48 @@ import java.util.List;
 public class ExampleController extends BaseRestController {
 
   ExampleService exampleService;
+  private PatientService patientService;
+  private VisitService visitService;
 
   @Autowired
-  public ExampleController(ExampleService exampleService) {
+  public ExampleController(ExampleService exampleService, PatientService patientService, VisitService visitService) {
     this.exampleService = exampleService;
+    this.patientService = patientService;
+    this.visitService = visitService;
   }
+
+
 
   @RequestMapping(method = RequestMethod.GET, value = "/patient/{uuid}", produces = "application/json")
   @ResponseBody
-  public List<Object> getDetails(@PathVariable("uuid") String patientUuid,
-                                          @RequestParam(value = "startDate", required = false) String startDateString,
-                                          @RequestParam(value = "endDate", required = false) String endDateString) throws ParseException {
-    return null;
+  public List getDetails(@PathVariable("uuid") String patientUuid,
+                         @RequestParam(value = "startDate", required = false) String startDateString,
+                         @RequestParam(value = "endDate", required = false) String endDateString) throws ParseException {
+
+
+    Patient patient = patientService.getPatientByUuid(patientUuid);
+    if (patient == null) {
+        //throw 404 error
+    }
+
+    List<Visit> activeVisits = visitService.getActiveVisitsByPatient(patient);
+    List<Order> orders = new ArrayList<>();
+    for (Visit activeVisit : activeVisits) {
+      activeVisit.getNonVoidedEncounters().stream().forEach(encounter -> orders.addAll(encounter.getOrders()));
+    }
+
+
+    List activeOrders = new ArrayList();
+    for (Order order : orders) {
+        HashMap<String, Object> orderInfo = new HashMap<>();
+        orderInfo.put("name", order.getConcept().getName().getName());
+        orderInfo.put("date", order.getDateActivated());
+        orderInfo.put("uuid", order.getConcept().getUuid());
+        activeOrders.add(orderInfo);
+
+    }
+
+    return activeOrders;
   }
 
 }
